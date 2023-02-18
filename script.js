70223159f435db531328024064eeb9a45813df85
subcylindrical <zacharylehouillier@gmail.com>
2023-02-18 11:14:28 -0800

    shift project to be an album searching tool, not a lyric searching tool

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function searchSongs(e) {
   searchBar.value = '';
 }
 
-// Toggle Lyrics Display
+// Toggle album Display
 function toggleLyrics(e) {
   console.log(e.target);
   if (e.target.classList.contains('result')) {
@@ -27,5 +27,20 @@ function toggleLyrics(e) {
 }
 
 function toggleLyricsClass() {
-  this.classList.toggle('show-lyrics');
+  this.classList.toggle('show-album');
 }
+
+// Fetch Song Data
+async function fetchSongs() {
+  const songData = await fetch(
+    'http://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=17ba43496b63af18f69826bc04d8b940&format=json'
+  );
+  //   const songData = await fetch(
+  //     'http://ws.audioscrobbler.com/2.0/?method=track.search&track=One&api_key=17ba43496b63af18f69826bc04d8b940&format=json'
+  //   );
+  const parsedSong = await songData.json();
+  console.log(parsedSong);
+  console.log(await parsedSong.results.albummatches.album[1]);
+}
+
+fetchSongs();
